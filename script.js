e91e96c999425bc910ec91f438c0e63bab819aff
Maksim <maksim.lashtur@mail.ru>
2021-02-28 20:43:16 +0200

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,44 @@
+'use strict',
+
+// 3, 4, 6;
+// 'string', "string", `string`;
+// true / false;
+// null
+// let a;
+// console.log(a);
+
+// let arr = ['ru', 'com', 'md'];
+// let obj = {
+//   name: 'hp',
+//   version: 1.2,
+//   year: 2992
+// }
+
+
+
+console.log(4 + "6");
+
+// console.log('https://vk.com/catalog/' + 'cars');
+
+console.log(typeof (Number('3')));
+
+console.log((parseInt("15px" + 10)));
+
+// let ans = +prompt("Hello", '');
+// console.log(ans);
+
+// alert("Привет!");
+
+// let answer = confirm("Вам есть 18?");
+
+// console.log(answer);
+
+// if (answer == false) {
+//   alert("bye bye!!!");
+// } else {
+//   alert("Good!")
+// }
+
+let answer = prompt("Вам есть 18?", "Да");
+
+console.log(typeof(answer));
